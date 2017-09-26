1c2f5c84687ccd76a85e9aeed7338adc3b0dddc0
Mikhail Tolstoy <festivalmg75@gmail.com>
2017-09-26 12:13:23 +0500

    use json

--- getSQLData.py
+++
@@ -1,8 +0,0 @@
-import sqlalchemy
-
-
-oracle_db = sqlalchemy.create_engine('oracle://username:password@database')
-connection = oracle_db.connect()
-result = connection.execute("SELECT test_column FROM test_table")
-for row in result:
-    print(row)
